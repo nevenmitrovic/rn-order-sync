44a4977d014a480e3e6d7961b8b1ddb0941d8e0d
neven <nevenmitrovic4@gmail.com>
2025-10-18 10:44:25 +0200

    update sign in form

--- components/auth/SignInForm.tsx
+++ components/auth/SignInForm.tsx
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from "react-native";
 import { borderRadius, colors, spacing, typography } from "@/constants/theme";
 import FormTextInput from "../common/FormTextInput";
 import MainButton from "../common/MainButton";
+import { Link } from "expo-router";
 
 export default function SignInForm() {
   return (
@@ -23,6 +24,12 @@ export default function SignInForm() {
           secureTextEntry
         />
         <MainButton buttonText="Sign in" onPress={() => {}} />
+        <View style={styles.navigationContainer}>
+          <Text>{`Don't`} have an account?</Text>
+          <Link style={styles.link} href={"/signup"}>
+            Sign Up
+          </Link>
+        </View>
       </View>
     </View>
   );
@@ -45,5 +52,15 @@ const styles = StyleSheet.create({
   form: {
     paddingTop: spacing.xl,
     gap: spacing.xxl,
+  },
+  link: {
+    fontWeight: "bold",
+    color: colors.colorAccent,
+  },
+  navigationContainer: {
+    flexDirection: "row",
+    gap: spacing.xs,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
